364a0e54fa21dc56a8c1471b40f6e48888e97853
Heidar Bernhardsson <heidar@iseld.org>
2013-09-26 00:01:40 -0700

    Move secret to a dotfile

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,19 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-DemoApp::Application.config.secret_key_base = '6fc9ab5f52c991c5e772de1253e3cc96aa9f5e460c29f831f0ffcfa4d74e7e4261e4a6cd1595efe50ead95a088d11d74d46178fc43d441c4e1b471942c233d15'
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+DemoApp::Application.config.secret_key_base = secure_token
